011f2c975367de65b4377650197b9cbb2cbdb470
Brian King <1854985+inflatador@users.noreply.github.com>
2018-02-07 11:35:39 -0600

    complete refactor; uses server image as argument instead of server now

--- rs-pv2hvm.py
+++ rs-pv2hvm.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # rc-pv2hvm, a script that converts Rackspace Cloud Servers from PV to HVM mode
-# version: 0.0.1a
+# version: 0.0.2a
 # Copyright 2018 Brian King
 # License: Apache
 
 import argparse
+import base64
+from collections import defaultdict
 import datetime
 from getpass import getpass
 import json
@@ -63,12 +65,12 @@ def get_auth_token(username,password):
     except requests.ConnectionError as e:
         print("Connection Error: Check your interwebs!")
         sys.exit()
-        
-    
+
+
     if r.status_code != 200:
         r = requests.post(url, headers=headers, json=payload2)
         if r.status_code != 200:
-            print ("Error! API responds with %d" % r.status_code) 
+            print ("Error! API responds with %d" % r.status_code)
             print("Rerun the script and you will be prompted to re-enter username/password.")
             wipe_keyring_credentials(username, password)
             sys.exit()
@@ -82,9 +84,10 @@ def get_auth_token(username,password):
     #assign token and account variables with info from json response.
     auth_token = data["access"]["token"]["id"]
     return auth_token
-    
+
 def find_endpoints(auth_token):
     #init Cloud Servers endpoints as an empty list
+    glance_endpoints=[]
     cs_endpoints=[]
     #setting up api call
     url = ("https://identity.api.rackspacecloud.com/v2.0/tokens/%s/endpoints" % auth_token)
@@ -94,97 +97,108 @@ def find_endpoints(auth_token):
     the_service_catalog = raw_service_catalog.json()
     endpoints = the_service_catalog["endpoints"]
     for service in range(len(endpoints)):
+        if "cloudImages" == endpoints[service]["name"]:
+            glance_endpoints.append(endpoints[service]["publicURL"])
         if "cloudServersOpenStack" == endpoints[service]["name"]:
-            cs_endpoints.append( endpoints[service]["publicURL"])
-    return cs_endpoints, headers
+            cs_endpoints.append(endpoints[service]["publicURL"])
+    return glance_endpoints, cs_endpoints, headers
 
-def find_cloud_server(auth_token, headers, cs_endpoints, cloud_server):
-    print ("Determining which region your cloud server is in...")
-    for endpoint in range(len(cs_endpoints)):
-        potential_url = ( "%s/servers/%s" % (cs_endpoints[endpoint], cloud_server) )
-        potential_server = requests.get(url=potential_url, headers=headers)
-        if potential_server.status_code == 200:
-            cs_object = potential_server
-            return cs_object
+def find_glance_image_and_cs_endpoint(auth_token, headers, cs_endpoints, glance_endpoints, glance_image):
+    print ("Determining which region your cloud server image is in...")
+    for endpoint in range(len(glance_endpoints)):
+        potential_url = ( "%s/images/%s" % (glance_endpoints[endpoint], glance_image) )
+        potential_image = requests.get(url=potential_url, headers=headers)
+        if potential_image.status_code == 200:
+            glance_object = potential_image
+            region = potential_url.split('//')[1].split('.')[0]
+            print ("Found image %s in %s region" % (glance_image, region))
             break
-    #if we make it this far, the cloud server UUID is invalid
-    print ("Error! Rackspace Cloud Server UUID %s was not found." % (cloud_server) )
+        for endpoint in cs_endpoints:
+            if region in endpoint:
+                cs_endpoint = endpoint
+    print cs_endpoint
+    return glance_object, cs_endpoint, region
+
+    #if we make it this far, the glance image UUID is invalid
+    print ("Error! Rackspace Cloud Server Image UUID %s was not found." % (glance_image) )
     sys.exit()
 
-def check_server_status(auth_token, headers, cs_object):
-#Ensure server is in the proper state before we take an image
-    vm_state = cs_object.json()["server"]["OS-EXT-STS:vm_state"]
-    if vm_state != "active":
-        print ("Error! Improper VM state. Expected 'active', got '%s'." % (vm_state))
+def check_glance_image(auth_token, headers, glance_image, glance_object):
+    # sanity checks
+    print ("Verifying image status...")
+    glance_status = (glance_object.json()["status"])
+    if glance_status != "active":
+        print ("Error! Wrong status for glance image %s. Expected 'active' ,\
+        but found '%s'" % (glance_image, glance_status))
         sys.exit()
-    task_state = cs_object.json()["server"]["OS-EXT-STS:task_state"]
-    if task_state is not None:
-        print ("Error! Improper task state. Expected 'None', got '%s'." % (task_state) )
-        sys.exit()
-#Ensure we aren't trying to image an OnMetal or BFV. This won't catch all of them as 
-# Performance, I/O and GP servers can also be BFV.
-    invalid_flavors = [ "metal", "memory", "compute" ]
-    for flavor in invalid_flavors:
-        if flavor in cs_object.json()["server"]["flavor"]["id"]:
-            print ("Error! Can't convert flavor '%s'." % (flavor) )
+    # vm_mode is not always set. Without it, servers will build in PV mode.
+    if "vm_mode" not in glance_object.json():
+        glance_object.json()["vm_mode"] = ""
+    else:
+        image_vm_mode = glance_object.json()["vm_mode"]
+        if image_vm_mode == "hvm":
+            print(
+            "Error! Image vm_mode already set to HVM, conversion not needed."
+            )
             sys.exit()
-        
-def create_server_image(auth_token, headers, cs_object):
-#image the server so we can build another one
-    print ("Creating image of server %s" % (cs_object.json()["server"]["name"]))
-    cs_url = ("%s/action" % (cs_object.json()["server"]["links"][0]["href"]))
-    image_creator = "rs-pv2hvm"
-    rand_postpend = str(uuid.uuid4())
-    image_name = ("%s-%s-%s" % (cs_object.json()["server"]["name"], image_creator, rand_postpend[0:7]))
-    data = {
-            "createImage" : {
-                "name" : image_name,
-                "metadata": {
-                    "created_by": image_creator
-
-                }
-            }
-            }
-    image_create = requests.post(url=cs_url, headers=headers,data=json.dumps(data))
-    image_create.raise_for_status()
-    if image_create.ok:
-        image_url = image_create.headers["Location"]
-        return image_url
-    #if we made it this far, somehow we never got the image URL.
-    if not image_url:
-        print ("Error! Did not receive image URL from Cloud Servers API. Exiting." )
+    # Confirm the OS is supported
+    image_os = (glance_object.json()["org.openstack__1__os_distro"])
+    image_version = (glance_object.json()["org.openstack__1__os_version"])
+    supported_os =('centos', 'redhat', 'ubuntu')
+    supported = False
+    for os in supported_os:
+        if os in image_os:
+            #Check RHEL/CentOS version. We don't do this with Ubuntu because
+            # release upgrades make this value unreliable
+            if image_os == 'com.redhat' or 'org.centos':
+                if "6." in image_version:
+                    supported = True
+                else:
+                    supported = False
+            supported = True
+    print (supported)
+    if supported == False:
+        print("Error! Image built from unsupported OS! Exiting.")
         sys.exit()
 
-def check_image_status(auth_token, headers, image_url):
-    image_info = requests.get(url=image_url, headers=headers)
-    image_status=image_info.json()["image"]["status"]
-    while image_status == "SAVING":
-        for x in range (0,100):
-            image_info = requests.get(url=image_url, headers=headers,stream=True)
-            image_status=image_info.json()["image"]["status"]
-            print ("Checking image status" + "." * x)
-            sys.stdout.write("\033[F")
-            print ("Image status is %s" % (image_status))
-            time.sleep(8)
-            if image_status() == "ACTIVE":
-            break
-#     print ("Checking image status of %s " % 
-    
-      
+def determine_server_flavor(auth_token, headers, glance_image, glance_object):
+    # for speed's sake, we will build General Purpose servers for Servers
+    min_disk = (glance_object.json()["min_disk"])
+    if min_disk <= 160:
+        disk_multiplier = 20
+        flavor_type = "general"
+        flavor_memory = str(min_disk / disk_multiplier)
+        flavor = flavor_type + "1-" + flavor_memory
+        return flavor
+    elif min_disk == 320:
+        flavor = 6
+        return flavor
+    elif min_disk == 620:
+        flavor = 7
+        return flavor
+    elif min_disk == 1200:
+        flavor = 8
+        return flavor
+    print ("Error! Could not determine flavor to use.")
+    sys.exit()
+
+def build_server(auth_token, headers, region, glance_image, glance_object, flavor):
+    cs_endpoint = "fooferal"
+# '{"server": {"name": "bking-d6-conv01", "imageRef": "23e4e7cb-5900-4544-a06c-338658dd8802", "key_name": "rackesc", "flavorRef": "performance1-1", "max_count": 1, "min_count": 1, "networks": [{"uuid": "29b682af-0ef3-4ba8-83c6-8e2ea8e6d7ee"}, {"uuid": "00000000-0000-0000-0000-000000000000"}, {"uuid": "11111111-1111-1111-1111-111111111111"}], "personality": [{"path": "/etc/rc.d/rc.local", "contents": "IyEvdXNyL2Jpbi9lbnYgYmFzaAojIERvd25sb2FkIHNjcmlwdCB0byBwZXJmb3JtIFBWIHRvIEhWTSBjb252ZXJzaW9uCiN0aGlzIGlzIGluamVjdGVkIGludG8gL2V0Yy9yYy5sb2NhbCBhdCBib290IHRpbWUKd2dldCAtcU8gL3RtcC9odm0uc2ggaHR0cDovL2U5NDJiMDI5YzI1NmVjMzIzMTM0LWQxNDA4Yjk2ODkyODU2MTgyMzEwOWNiNjZjNDdlYmNkLnIzNy5jZjUucmFja2Nkbi5jb20vaHZtLnNoCi91c3IvYmluL2VudiBiYXNoIC90bXAvaHZtLnNoCiNPbmx5IHVzZWQgZm9yIHRyb3VibGVzaG9vdGluZywgZG9lcyBub3QgbmVlZCB0byBiZSBpbiBmaW5hbCBzY3JpcHQKc3NoLWtleWdlbiAtQQ=="}, {"path": "/etc/rc.local", "contents": "IyEvdXNyL2Jpbi9lbnYgYmFzaAojIERvd25sb2FkIHNjcmlwdCB0byBwZXJmb3JtIFBWIHRvIEhWTSBjb252ZXJzaW9uCiN0aGlzIGlzIGluamVjdGVkIGludG8gL2V0Yy9yYy5sb2NhbCBhdCBib290IHRpbWUKd2dldCAtcU8gL3RtcC9odm0uc2ggaHR0cDovL2U5NDJiMDI5YzI1NmVjMzIzMTM0LWQxNDA4Yjk2ODkyODU2MTgyMzEwOWNiNjZjNDdlYmNkLnIzNy5jZjUucmFja2Nkbi5jb20vaHZtLnNoCi91c3IvYmluL2VudiBiYXNoIC90bXAvaHZtLnNoCiNPbmx5IHVzZWQgZm9yIHRyb3VibGVzaG9vdGluZywgZG9lcyBub3QgbmVlZCB0byBiZSBpbiBmaW5hbCBzY3JpcHQKc3NoLWtleWdlbiAtQQ=="}]}}'
 
 #begin main function
 @plac.annotations(
-    cloud_server=plac.Annotation("UUID of Rackspace Cloud Server")
+    glance_image=plac.Annotation("UUID of Cloud Server image")
                 )
 
-def main(cloud_server):
+def main(glance_image):
     username,password = getset_keyring_credentials()
     auth_token = get_auth_token(username, password)
-    cs_endpoints, headers = find_endpoints(auth_token)
-    cs_object = find_cloud_server(auth_token, headers, cs_endpoints, cloud_server)
-    check_server_status(auth_token, headers, cs_object)
-    image_url = create_server_image(auth_token, headers, cs_object)
-    check_image_status(auth_token, headers, image_url)
+    glance_endpoints, cs_endpoints, headers = find_endpoints(auth_token)
+    glance_object, cs_endpoint, region = find_glance_image_and_cs_endpoint(auth_token, headers, cs_endpoints, glance_endpoints, glance_image)
+    check_glance_image(auth_token, headers, glance_image, glance_object)
+    flavor = determine_server_flavor(auth_token, headers, glance_image, glance_object)
+    build_server(auth_token, headers, region, glance_image, glance_object, flavor)
 
 if __name__ == '__main__':
     import plac
